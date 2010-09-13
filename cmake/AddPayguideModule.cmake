eedb7f168fb2b7ca1f86160ce9f5b001c6b7c872
Alexander <alex@broadband-77-37-222-49.nationalcablenetworks.ru>
2010-09-14 02:53:00 +0400

    missed file added

---
+++ cmake/AddPayguideModule.cmake
@@ -0,0 +1,43 @@
+FUNCTION(ADD_PAYGUIDE_MODULE MODNAME classname)
+MESSAGE("${CMAKE_CURRENT_SOURCE_DIR}")
+
+SET(PG_MODULE_CLASS ${classname})
+SET(SKELETON "PayguideModuleSkeleton.cpp")
+SET(MODPATH "${CMAKE_CURRENT_SOURCE_DIR}/${MODNAME}")
+MESSAGE("Configuring ${CMAKE_CURRENT_SOURCE_DIR}/${SKELETON}.stub -> ${CMAKE_CURRENT_SOURCE_DIR}/${MODNAME}/${SKELETON}")
+CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/${SKELETON}.stub" "${MODPATH}/${SKELETON}")
+ADD_SUBDIRECTORY(webmoney)
+
+SET(PG_TARGET "pg_${MODNAME}")
+INCLUDE("${MODPATH}/include")
+ADD_LIBRARY (${PG_TARGET} SHARED 
+	${MODPATH}/${SKELETON}
+	${PAYGUIDE_SRC}/obj/core.o
+	${PAYGUIDE_SRC}/obj/paysys.o
+	${PAYGUIDE_SRC}/obj/sem.o
+	${PAYGUIDE_SRC}/obj/operator.o
+	${PAYGUIDE_SRC}/obj/parser.o
+	${PAYGUIDE_SRC}/obj/log.o
+	)
+
+TARGET_LINK_LIBRARIES(${PG_TARGET}
+	${MODNAME}
+	upgm
+	pthread
+	)
+
+
+INSTALL(TARGETS ${PG_TARGET} DESTINATION ${PAYGUIDE_SRC}/modules/paysys)
+INSTALL(FILES ${MODPATH}/config/lib${PG_TARGET}.so.cfg.example DESTINATION ${PAYGUIDE_SRC}/modules/paysys)
+
+INSTALL(FILES
+	${MODPATH}/config/config.cfg.example
+	${MODPATH}/config/scheme.cfg.example
+	${MODPATH}/config/codes.cfg.example
+       	DESTINATION /etc/upgm/${MODNAME})
+
+INSTALL(FILES
+	${MODPATH}/config/${MODNAME}.op.example
+       	DESTINATION ${PAYGUIDE_SRC}/operators)
+
+ENDFUNCTION(ADD_PAYGUIDE_MODULE)
